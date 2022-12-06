7e107b563747ad02e2a0a7d701f514ae8244b374
Slater McArdle <slater.mcardle@outlook.com>
2022-12-05 20:46:56 -0800

    Day 5 solutions completed

---
+++ Day-5/part-one-solution.js
@@ -0,0 +1,65 @@
+// Import necessary modules
+const fs = require("fs");
+
+// Get the data from the input file
+const input = fs.readFileSync("input.txt");
+
+// Split the data into two arrays at the double new line
+const data = input.toString().split("\r\n\r\n");
+
+// Split the stacks and steps into arrays at each newline
+let stacks = data[0].split("\r\n");
+const steps = data[1].split("\r\n");
+
+// Create a placeholder array for the processed horizontal stacks
+let finalStacks = [];
+
+// Process the stacks, breaking them into crates and removing the surrounding characters
+for (let i = 0; i < stacks.length; i++) {
+    let stack = stacks[i].match(/.{1,4}/g);
+    let trimmedStack = [];
+    for (let j = 0; j < stack.length; j++) {
+        trimmedStack.push(stack[j][1]);
+    }
+    finalStacks.push(trimmedStack);
+}
+
+// Remove the stack numbers
+finalStacks.pop();
+
+// Create a placeholder for the vertical stacks
+let transformedStacks = [];
+
+// Transform the horizontal stacks into vertical stacks
+for (let i = 0; i < finalStacks[0].length; i++) {
+    let insideStack = [];
+    for (let j = 0; j < finalStacks.length; j++) {
+        if (finalStacks[j][i] !== ' ') {
+            insideStack.unshift(finalStacks[j][i]);
+        }
+    }
+    transformedStacks.push(insideStack);
+}
+
+// Loop through each step and move the crates
+for (let i = 0; i < steps.length; i++) {
+    // Split the steps apart and grab the amount and column numbers
+    let stepArray = steps[i].split(' ');
+    const amount = parseInt(stepArray[1]);
+    const initialStack = parseInt(stepArray[3]) - 1;
+    const finalStack = parseInt(stepArray[5]) - 1;
+    // Move the top crate from the starting to ending column the right amount of times
+    for (let j = 0; j < amount; j++) {
+        let poppedValue = transformedStacks[initialStack].pop();
+        console.log(poppedValue);
+        transformedStacks[finalStack].push(poppedValue);
+    }
+}
+
+// Grab the values of each of the top crates
+let topCrates = '';
+transformedStacks.forEach((stack) => {
+    topCrates += stack[stack.length - 1];
+})
+
+console.log(topCrates);
